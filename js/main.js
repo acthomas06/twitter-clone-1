f6e34cd54406bf190bd9513e5aca0d92d25f46e6
Andrew Thomas <acthomas06@github.com>
2014-01-18 13:58:34 -0700

    Adds twitter elements and jquery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 $(function(){
 
-	$('#tweet-controls').hide();
 
+	/**	TOOLTIPS SECTION**/
+
+		$('.avatar').attr('data-toggle','tooltip');
+		$('.avatar').tooltip({
+			placement: 'left',
+			title: function() {
+				return "" + $('.tweet').find('.fullname').text() + "";
+			}
+		});
+
+	/** HIDE TWEET CONTROLS **/
+	$('#tweet-controls').hide();
+	$('#tweet-submit').attr('disabled', true);
+
+
+	/** EXPAND TWEET COMPOSE **/
 	$('.tweet-compose').on('click', function() {
 		$(this).addClass('expand');
 		$('#tweet-controls').show();
 	});
+
 
 	$('.tweet-compose').on('keyup', function() {
 		var length = $(this).val().length;
@@ -20,7 +36,7 @@ $(function(){
 			$('#char-count').css('color', '#999');
 		}
 
-		if(length > 140) 
+		if(length > 140 || length === 0) 
 		{
 			$('#tweet-submit').attr('disabled', true);
 		}
@@ -32,15 +48,34 @@ $(function(){
 
 	$('#tweet-submit').on('click', function() {
 		var length = $('.tweet-compose').val().length;
-		var profile = $('#profile-summary').clone();
-		var content = $('.tweet-compose');
-		if(length === 0)
-		{
-			alert("You can't submit an empty tweet!");
-		}
-		else
-		{
-			$('#stream').prepend(profile, content.val());
-		}
+		var content = $('.tweet-compose').val();
+		var myName = $('#profile-summary > .content > .fullname').text();
+		var myUser = $('#profile-summary > .content > .username').text();
+		var tweet = $('.tweet-text');
+		var fullName = $('.fullname');
+		var userName = $('.username');
+
+	
+		var newTweet = $('.tweet').eq(0).clone();
+
+		newTweet.find('.avatar').attr('src','img/alagoon.jpg');
+		newTweet.find('.fullname').text(myName);
+		newTweet.find('.username').text(myUser);
+		newTweet.find('.tweet-text').text(content);
+
+		newTweet.prependTo('#stream');
+
+		$('.tweet-compose').val("");
+		$('.tweet-compose').removeClass('expand');
+
 	});
+
+	$('.tweet').on('click', function() {
+		$(this).find('.stats').animate({
+			height: 'toggle'
+		}, 500);
+		$(this).find('.reply').show();
+	});
+
+	$('.time').timeago();
 });
